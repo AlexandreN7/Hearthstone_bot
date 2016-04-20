8df71d27f14fe3cef4c73103d7dc832f2511efee
archi <archi@BigDaddy.home>
2016-04-20 23:06:29 +0200

    repository update

--- src/get_database.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-rm database_buf.json
-curl --get --include 'https://omgvamp-hearthstone-v1.p.mashape.com/cards' \
-  -H 'X-Mashape-Key: fKjbakCDMymshCbUBwGgQLr7hlhEp1gWIYdjsn4iPqtgINyrl3' > database_buf.json
